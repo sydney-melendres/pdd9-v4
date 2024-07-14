a979546f439ba3b81bc8800a654ecfa67a96535d
Sydney Melendres <sydneymelendres@gmail.com>
2024-07-14 22:26:41 +1000

    send help

--- processes/12_additional_counters.py
+++ processes/12_additional_counters.py
@@ -10,35 +10,48 @@ output_path = 'final-data/player_performance.csv'
 df = pd.read_csv(input_path)
 
 # Drop the 'log_line' column
-df.drop(columns=['log_line'], inplace=True)
+drop = {
+    'log_line',
+    'killer_id',
+    'victim_id',
+    'weapon_id',
+    'weapon',
+    'player_id',
+    'log_score'
+}
+df.drop(columns=['{drop}'], inplace=True)
 
 # Adding the new columns with initial values set to 0
 df['suicide_count'] = 0
 df['deaths_total'] = 0
-# df['killed_by_Player_172.19.137.208'] = 0
-# df['killed_by_Player_172.19.114.48'] = 0
-# df['killed_by_Player_172.19.119.51'] = 0
-# df['killed_by_Player_172.19.116.18'] = 0
-# df['killed_by_Player_172.19.120.104'] = 0
-# df['killed_by_Player_172.19.117.18'] = 0
+
+players = [
+    '172.19.137.208', '172.19.114.48', '172.19.119.51', 
+    '172.19.116.18', '172.19.120.104', '172.19.117.18'
+]
+
+for player in players:
+    df[f'killed_by_Player_{player}'] = 0
+    df[f'killed_Player_{player}'] = 0
 
 # Function to calculate the counts for each round
 def calculate_counts(group):
     suicide_count = 0
-    # deaths_by_ip = {ip: 0 for ip in [
-    #     'Player_172.19.137.208', 'Player_172.19.114.48', 'Player_172.19.119.51',
-    #     'Player_172.19.116.18', 'Player_172.19.120.104', 'Player_172.19.117.18'
-    # ]}
-    
+
     # Mark suicides
     group['is_suicide'] = group['killer_ip'] == group['victim_ip']
     
     # Calculate cumulative suicides
-    group['suicide_count'] = group.groupby('victim_ip')['is_suicide'].cumsum()
-        
+    group['suicide_count'] = group.groupby('player_ip')['is_suicide'].cumsum()
         
     # Calculate deaths total
     group['deaths_total'] = group.groupby('victim_ip').cumcount() + 1
+    
+    # Update killed_by_Player and killed_Player counters
+    for player in players:
+        player_ip = f'Player_{player}'
+        group[f'killed_by_Player_{player}'] = (group['killer_ip'] == player_ip).cumsum()
+        group[f'killed_Player_{player}'] = (group['victim_ip'] == player_ip).cumsum()
     
     return group
 
